e669d49e6637703b8ffda15c3261c4282c76ddc8
Jariullah <safijari@isu.edu>
2022-03-05 13:01:33 +0100

    ?

--- helpers.py
+++ helpers.py
@@ -1,10 +1,13 @@
 import pygfx as gfx
-from wgpu.gui.auto import WgpuCanvas, run
+# from wgpu.gui.auto import WgpuCanvas, run
+from wgpu.gui.offscreen import WgpuCanvas, run
+from abc import ABC, abstractmethod
+import glfw
 
 
-class StandardScene:
-    def __init__(self, perspective=True, animate_fn=None):
-        self.canvas = WgpuCanvas()
+class StandardScene(ABC):
+    def __init__(self, perspective=True):
+        self.canvas = WgpuCanvas(max_fps=1000)
         self.renderer = gfx.renderers.WgpuRenderer(self.canvas)
         self.scene = gfx.Scene()
         self.camera = (
@@ -14,15 +17,23 @@ class StandardScene:
         )
         self.controls = gfx.OrbitControls()
         self.controls.add_default_event_handlers(self.canvas, self.camera)
-        self.animate_fn = animate_fn
+
+    @abstractmethod
+    def setup(self):
+        pass
+
+    @abstractmethod
+    def animate_fn(self):
+        pass
 
     def animate(self):
-        assert self.animate_fn is not None
         self.animate_fn()
         self.controls.update_camera(self.camera)
         self.renderer.render(self.scene, self.camera)
+        print("got here")
         self.canvas.request_draw()
 
     def start(self):
+        self.setup()
         self.canvas.request_draw(self.animate)
         run()
--- main.py
+++ main.py
@@ -2,22 +2,31 @@ import time
 import numpy as np
 from helpers import StandardScene
 import pygfx as gfx
+import trimesh
 
-ss = StandardScene()
-
-# Populate a scene with a cube
-scene = ss.scene
-geometry = gfx.box_geometry(200, 200, 200)
-material = gfx.MeshPhongMaterial(color=(1, 1, 0, 1))
-cube = gfx.Mesh(geometry, material)
-scene.add(cube)
+import imageio
 
 
-def update():
-    rot = gfx.linalg.Quaternion().set_from_euler(gfx.linalg.Euler(0.005, 0.01))
-    cube.rotation.multiply(rot)
+class Test(StandardScene):
+    def setup(self):
+        self.keycap_geom = gfx.trimesh_geometry(
+            trimesh.load("/home/jari/Downloads/xda_keycap_v5.stl")
+        )
+        im = imageio.imread("imageio:bricks.jpg")
+        self.tex = gfx.Texture(im, dim=2).get_view(filter="linear", address_mode="repeat")
+        # self.keycap_geom.texcoords.data[:, 0] *= 10  # stretch the texture
+        self.keycap = gfx.Mesh(self.keycap_geom, gfx.MeshPhongMaterial(color="red"))
+        self.scene.add(self.keycap)
+        self.prev_time = time.time()
+
+    def animate_fn(self):
+        # rot = gfx.linalg.Quaternion().set_from_euler(gfx.linalg.Euler(0.005, 0.01))
+        # self.cube.rotation.multiply(rot)
+        print(time.time() - self.prev_time)
+        self.prev_time = time.time()
 
 
-ss.animate_fn = update
+Test().start()
 
-ss.start()
+while True:
+    pass
